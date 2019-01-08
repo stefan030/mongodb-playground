f424c1370a0baa76b45a840768cb7d8043891122
stefan <stefanbogdanovic030@gmail.com>
2019-01-08 12:50:37 +0100

    Find records by ID

--- test/finding_test.js
+++ test/finding_test.js
@@ -5,11 +5,14 @@ const connection = require('../connection.js');
 
 
 // Describe tests e.g. 'connect to db'...
-describe('Finding records', (done) => {
+describe('Finding records', () => {
+
+    // Define variable globally so we have full access to it, even outside of beforeEach
+    var char;
 
     beforeEach((done) => {
         // New instance of a MarioChar using MarioChar model
-        var char = new MarioChar({
+        char = new MarioChar({
             name: 'Mario'
         });
 
@@ -27,6 +30,19 @@ describe('Finding records', (done) => {
 
         MarioChar.findOne({name: 'Mario'}).then((result) => {
             assert(result.name === 'Mario');
+            done();
+        });
+
+    });
+
+    // Next test goes here
+
+    it('Finds one record by ID from the database', (done) => {
+
+        // Mongoose .findOne does not require .toString as the Mongoose is cleaver enough to understand that we want to find a record with same ID
+        MarioChar.findOne({_id: char._id}).then((result) => {
+            // assert requires toString() so we get the actual string of that object ID (check Robomongo for more details about type of record)
+            assert(result._id.toString() == char._id.toString());
             done();
         });
 
